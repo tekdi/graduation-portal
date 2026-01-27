ec093f2b14bd3bca22fab8e0eca5a6f4451c9dfb
Tejashri Majage <tejashri_m@techjoomla.com>
2026-01-27 14:56:24 +0530

    UT fixes

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -20,13 +20,8 @@ const WelcomePage: React.FC = () => {
       alignItems="center"
       bg={theme.tokens.colors.error50}
     >
-      <Container>
-        <VStack
-          space="2xl"
-          alignItems="center"
-          marginHorizontal="$0"
-          $md-marginHorizontal="$12"
-        >
+      <Container padding="$5" $md-padding="$6">
+        <VStack space="2xl" alignItems="center">
           {/* Welcome Header */}
           <VStack
             space="sm"
